c4110d8439f07679efca97d85c63e00b956a74bc
Rehan <75390754+Rehans422@users.noreply.github.com>
2026-04-06 17:33:16 +0100

    Completion of Part 1
    The project was created using the webapp archetype from Maven.
    
    The api was created in SmartCampusPath.java and the discovery endpoint created in the DiscoveryResource.java file

---
+++ Smart-Campus-api/src/main/java/com/mycompany/smart/campus/api/SmartCampusPath.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smart.campus.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author rsoha
+ */
+@ApplicationPath("/api/v1")
+public class SmartCampusPath extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(DiscoveryResource.class);
+        return classes;
+    }
+}
