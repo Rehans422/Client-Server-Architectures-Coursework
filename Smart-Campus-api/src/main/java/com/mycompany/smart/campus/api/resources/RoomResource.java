e6c7e0596148e7a212c4ee366fb93ba8aa8fea4a
Rehan <75390754+Rehans422@users.noreply.github.com>
2026-04-10 18:19:36 +0100

    Completion of part 2.2

--- Smart-Campus-api/src/main/java/com/mycompany/smart/campus/api/resources/RoomResource.java
+++ Smart-Campus-api/src/main/java/com/mycompany/smart/campus/api/resources/RoomResource.java
@@ -7,9 +7,13 @@ package com.mycompany.smart.campus.api.resources;
 import java.util.List;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 import com.mycompany.smart.campus.api.dao.MockDatabase;
 import com.mycompany.smart.campus.api.dao.GenericDAO;
 import com.mycompany.smart.campus.api.models.RoomModel;
+import java.net.URI;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -25,17 +29,35 @@ public class RoomResource {
     public List<RoomModel> roomList() {
         return sensorDAO.getAll();
     }
-    
+
     @GET
     @Path("/{roomId}")
     @Produces(MediaType.APPLICATION_JSON)
     public RoomModel getRoomById(@PathParam("roomId") int roomId) {
         return sensorDAO.getById(roomId);
     }
-    
+
+    @Context
+    private UriInfo uriInfo;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addRoom(RoomModel room) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addRoom(RoomModel room) {
         sensorDAO.add(room);
+
+        URI newRoomUri = uriInfo.getAbsolutePathBuilder()
+                .path(room.getId())
+                .build();
+
+        return Response.created(newRoomUri).build();
+    }
+
+    @DELETE
+    @Path("/{roomId}")
+    public String deleteRoom(@PathParam("roomId") String id) {
+        sensorDAO.delete(id);
+
+        return "Room with id = " + id + " has been deleted.";
     }
 }
